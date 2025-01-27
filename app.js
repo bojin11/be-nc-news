ff16e17df88cdf87ce7ce1538c4e171f8dd8e630
Bojin Zhou <bojinz511@gmail.com>
2025-01-27 13:40:44 +0000

    02-get-topics complete

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 const express = require("express");
-const { getEndPoints } = require("./controllers/controllers");
+const { getEndPoints, getTopics } = require("./controllers/controllers");
 const app = express();
 
 app.get("/api", getEndPoints);
+app.get("/api/topics", getTopics);
+
+app.use((err, req, res, next) => {
+  console.error("Error stack:", err.stack);
+  res.status(500).send("Internal Server Error");
+});
 
 module.exports = app;
--- controllers/controllers.js
+++ controllers/controllers.js
@@ -1,6 +1,17 @@
-const { fetchEndPoints } = require("../models/models");
+const { fetchEndPoints, fetchTopics } = require("../models/models");
 
 exports.getEndPoints = (req, res) => {
   const result = fetchEndPoints();
   res.send({ endpoints: result });
+};
+
+exports.getTopics = (req, res, next) => {
+  fetchTopics()
+    .then((result) => {
+      res.send({ topics: result });
+    })
+    .catch((err) => {
+      console.error("Error fetching topics:", err);
+      next(err);
+    });
 };
