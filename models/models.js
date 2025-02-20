e50e7e702c1ec2134b8a44ec3ecb29489f185033
Bojin Zhou <bojinz511@gmail.com>
2025-02-20 16:58:10 +0000

    deleted unwanted code

--- models/models.js
+++ models/models.js
@@ -51,39 +51,6 @@ exports.fetchArticleFromArticleId = (article_id) => {
   });
 };
 
-// exports.fetchArticles = ({ sort_by = "created_at", order = "desc", topic }) => {
-//   let SQLString = "SELECT * FROM articles";
-//   args = [];
-
-//   if (topic) {
-//     SQLString += ` WHERE topic = $2`;
-//     args.push(topic);
-//   }
-
-//   const greenList = [
-//     "title",
-//     "topic",
-//     "author",
-//     "body",
-//     "created_at",
-//     "votes",
-//     "article_img_url",
-//   ];
-
-//   if (greenList.includes(sort_by)) {
-//     SQLString += ` ORDER BY $1 ${order === "desc" ? "DESC" : "ASC"}`;
-//     args.unshift(sort_by);
-//   }
-
-//   return db.query(SQLString, args).then((result) => {
-//     if (result.length === 0) {
-//       return Promise.reject();
-//     } else {
-//       return result.rows;
-//     }
-//   });
-// };
-
 exports.fetchArticles = ({ sort_by = "created_at", order = "desc", topic }) => {
   let SQLString = `SELECT * FROM articles`;
   const args = [];
